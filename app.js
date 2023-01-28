8ec44b5279f969424b63cdab1f2c254ddc1ea048
ishaaty <ishatyagi888@gmail.com>
2023-01-27 20:51:34 -0500

    connected database to front-end stuff

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require( "express" );
 const app = express();
 const port = 8080;
 const logger = require("morgan");
+const db = require('./db/db_connection');
+let data;
+
+// Configure Express to use EJS
+app.set( "views",  __dirname + "/views");
+app.set( "view engine", "ejs" );
 
 // define middleware that logs all incoming requests
 app.use(logger("dev"));
@@ -10,20 +16,47 @@ app.use(express.static(__dirname + '/public'));
 
 // define a route for the default home page
 app.get( "/", ( req, res ) => {
-    res.sendFile( __dirname + "/views/index.html" );
+    res.render("index");
 } );
 
 // define a route for the stuff inventory page
+const read_timer_all_sql = `
+    SELECT 
+        id, task, estimated_time
+    FROM
+        tasks
+`
 app.get( "/timer", ( req, res ) => {
-    res.sendFile( __dirname + "/views/timer.html" );
-} );
+    db.execute(read_timer_all_sql, (error, results) => {
+        if (error)
+            res.status(500).send(error); // Internal Server Error
+        else
+            res.render('timer', {tasks: results});
+    });
+});
 
 // define a route for the item detail page
-app.get( "/timer/task", ( req, res ) => {
-    res.sendFile( __dirname + "/views/task.html" );
-} );
+const read_task_sql = `
+    SELECT 
+        task, estimated_time, description 
+    FROM
+        tasks
+    WHERE
+        id = ?
+`
+app.get( "/timer/task/:id", ( req, res, next ) => {
+    db.execute(read_task_sql, [req.params.id], (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else if (results.length == 0)
+            res.status(404).send(`No item found with id = "${req.params.id}"` ); // NOT FOUND
+        else
+            data = results[0]; // results is still an array
+            res.render('task', data);
+    });
+});
 
 // start the server
 app.listen( port, () => {
     console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
-} );
+});
--- db/db_connection.js
+++ db/db_connection.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
+// const db = require('./db/db_connection');
 
 dotenv.config();
 
